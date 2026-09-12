6452095b6f1848eade2db5923c71cc8ba1417634
Vishwatej <r.vishwatej7@gmail.com>
2026-09-12 22:54:47 +0530

    bigram

--- src/main.py
+++ src/main.py
@@ -1,11 +1,38 @@
-text = "I love machine learning"
+sentences = [
+    "I like cats",
+    "I like dogs",
+    "I like pizza",
+    "I like cats"
+]
 
-tokens = text.lower().split()
+bigram_counts = {}
+context_counts = {}
 
-bigrams = []
+for sentence in sentences:
+    tokens = sentence.lower().split()
 
-for i in range(len(tokens) - 1):
-    bigram = (tokens[i], tokens[i + 1])
-    bigrams.append(bigram)
+    for i in range(len(tokens) - 1):
+        current_word = tokens[i]
+        next_word = tokens[i + 1]
 
-print(bigrams)
+        bigram = (current_word, next_word)
+
+        # Count the bigram
+        if bigram not in bigram_counts:
+            bigram_counts[bigram] = 0
+
+        bigram_counts[bigram] += 1
+
+        # Count the context word
+        if current_word not in context_counts:
+            context_counts[current_word] = 0
+
+        context_counts[current_word] += 1
+
+print("Bigram counts:")
+print(bigram_counts)
+
+print()
+
+print("Context counts:")
+print(context_counts)
